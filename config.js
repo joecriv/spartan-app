3cdec1e25f112243edf7379f968c5d464841b765
joecriv <joseph@spartaninstallations.com>
2026-04-12 14:31:52 -0400

    Set Spartan shop ID

--- config.js
+++ config.js
@@ -19,8 +19,7 @@ const BRAND = {
     supabaseUrl:  'https://lhmafiizghcfefrohtvm.supabase.co',
     supabaseKey:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxobWFmaWl6Z2hjZmVmcm9odHZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5OTM4ODIsImV4cCI6MjA5MTU2OTg4Mn0.RQ_hy9ID-99wYUvopQfC27RP-E8iZQ5faSG9-Qal-KQ',
 
-    // Will be set after running: insert into shops (name, max_seats) values ('Spartan', 3);
-    shopId: null,
+    shopId: '890f5923-2da8-4202-af75-a99721ac4df6',
 
 };
 
